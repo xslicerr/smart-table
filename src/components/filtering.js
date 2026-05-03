6c66ad31e11a33ac386969b30c0f5aaa098565a0
Nikita Korchagin <slicerghost1@yandex.ru>
2026-05-03 23:09:38 +0400

    first commit

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,35 @@
+
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {                        // используйте name как значение и текстовое содержимое
+                            const newName = document.createElement('option');
+                            newName.value = name;
+                            newName.textContent = name;
+                            return newName;// @todo: создать и вернуть тег опции
+                      })
+        )
+     })
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const field = action.dataset.field;
+            const input = action.parentElement.querySelector('input');
+            input.value = '';
+            state[field] = '';
+        }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/table.js
+++ src/components/table.js
@@ -5,19 +5,48 @@ import {cloneTemplate} from "../lib/utils.js";
  *
  * @param {Object} settings
  * @param {(action: HTMLButtonElement | undefined) => void} onAction
- * @returns {{container: Node, elements: *, render: render}}
+ * @returns {{conтакtainer: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
-
+    before.reverse().forEach(subName => {                            // перебираем нужный массив идентификаторов
+        root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+        root.container.prepend(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+    });
+    after.forEach(subName => {                            // перебираем нужный массив идентификаторов
+        root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+        root.container.append(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+    });
     // @todo: #1.3 —  обработать события и вызвать onAction()
+
+    root.container.addEventListener('change', () => {
+        onAction();
+    })
+
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction)
+    })
+
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    })
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => {
+                if (key in row.elements) {
+                    row.elements[key].textContent = item[key];
+                }
+            })
+
+            return row.container
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
