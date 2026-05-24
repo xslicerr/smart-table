ed80ebb893945935b8c166fa8e9f9b91a3874908
Nikita Korchagin <slicerghost1@yandex.ru>
2026-05-24 06:55:48 +0400

    Переделаны все модули

--- src/data.js
+++ src/data.js
@@ -1,58 +1,61 @@
-import {makeIndex} from "./lib/utils.js";
-const BASE_URL = 'https://webinars.webdev.education-services.ru/sp7-api';
-export function initData() {
-        // переменные для кеширования данных
-    let sellers;
-    let customers;
-    let lastResult;
-    let lastQuery;
+const BASE_URL = "https://webinars.webdev.education-services.ru/sp7-api";
 
-    // функция для приведения строк в тот вид, который нужен нашей таблице
-    const mapRecords = (data) => data.map(item => ({
-        id: item.receipt_id,
-        date: item.date,
-        seller: sellers[item.seller_id],
-        customer: customers[item.customer_id],
-        total: item.total_amount
+export function initData() {
+  // переменные для кеширования данных
+  let sellers;
+  let customers;
+  let lastResult;
+  let lastQuery;
+
+  // функция для приведения строк в тот вид, который нужен нашей таблице
+  const mapRecords = (data) =>
+    data.map((item) => ({
+      id: item.receipt_id,
+      date: item.date,
+      seller: sellers[item.seller_id],
+      customer: customers[item.customer_id],
+      total: item.total_amount,
     }));
 
-    // функция получения индексов
-    const getIndexes = async () => {
-        if (!sellers || !customers) { // если индексы ещё не установлены, то делаем запросы
-            [sellers, customers] = await Promise.all([ // запрашиваем и деструктурируем в уже объявленные ранее переменные
-                fetch(`${BASE_URL}/sellers`).then(res => res.json()), // запрашиваем продавцов
-                fetch(`${BASE_URL}/customers`).then(res => res.json()), // запрашиваем покупателей
-            ]);
-        }
-
-        return { sellers, customers };
+  // функция получения индексов
+  const getIndexes = async () => {
+    if (!sellers || !customers) {
+      // если индексы ещё не установлены, то делаем запросы
+      [sellers, customers] = await Promise.all([
+        // запрашиваем и деструктурируем в уже объявленные ранее переменные
+        fetch(`${BASE_URL}/sellers`).then((res) => res.json()), // запрашиваем продавцов
+        fetch(`${BASE_URL}/customers`).then((res) => res.json()), // запрашиваем покупателей
+      ]);
     }
 
-    // функция получения записей о продажах с сервера
-    const getRecords = async (query, isUpdated = false) => {
-            const qs = new URLSearchParams(query); // преобразуем объект параметров в SearchParams объект, представляющий query часть url
-            const nextQuery = qs.toString(); // и приводим к строковому виду
+    return { sellers, customers };
+  };
 
-            if (lastQuery === nextQuery && !isUpdated) { // isUpdated параметр нужен, чтобы иметь возможность делать запрос без кеша
-                return lastResult; // если параметры запроса не поменялись, то отдаём сохранённые ранее данные
-            }
+  // функция получения записей о продажах с сервера
+  const getRecords = async (query, isUpdated = false) => {
+    const qs = new URLSearchParams(query); // преобразуем объект параметров в SearchParams объект, представляющий query часть url
+    const nextQuery = qs.toString(); // и приводим к строковому виду
 
-            // если прошлый квери не был ранее установлен или поменялись параметры, то запрашиваем данные с сервера
-            const response = await fetch(`${BASE_URL}/records?${nextQuery}`);
-            const records = await response.json();
+    if (lastQuery === nextQuery && !isUpdated) {
+      // isUpdated параметр нужен, чтобы иметь возможность делать запрос без кеша
+      return lastResult; // если параметры запроса не поменялись, то отдаём сохранённые ранее данные
+    }
 
-            lastQuery = nextQuery; // сохраняем для следующих запросов
-            lastResult = {
-                total: records.total,
-                items: mapRecords(records.items)
-            };
+    // если прошлый квери не был ранее установлен или поменялись параметры, то запрашиваем данные с сервера
+    const response = await fetch(`${BASE_URL}/records?${nextQuery}`);
+    const records = await response.json();
 
-            return lastResult;
-        };
-
-    return {
-        getIndexes,
-        getRecords
+    lastQuery = nextQuery; // сохраняем для следующих запросов
+    lastResult = {
+      total: records.total,
+      items: mapRecords(records.items),
     };
-    
+
+    return lastResult;
+  };
+
+  return {
+    getIndexes,
+    getRecords,
+  };
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
-import './fonts/ys-display/fonts.css'
-import './style.css'
+import "./fonts/ys-display/fonts.css";
+import "./style.css";
 
-import {data as sourceData} from "./data/dataset_1.js";
+import { initData } from "./data.js";
+import { processFormData } from "./lib/utils.js";
 
-import {initData} from "./data.js";
-import {processFormData} from "./lib/utils.js";
-
-import {initTable} from "./components/table.js";
-import { initPagination } from './components/pagination.js';
-import { initSorting } from './components/sorting.js';
-import { initFiltering } from './components/filtering.js'
-import { initSearching } from './components/searching.js';
+import { initTable } from "./components/table.js";
+import { initPagination } from "./components/pagination.js";
+import { initSorting } from "./components/sorting.js";
+import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 // @todo: подключение
-
 
 // Исходные данные используемые в render()
 const api = initData();
@@ -22,18 +19,19 @@ const api = initData();
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
-    const rowsPerPage = parseInt(state.rowsPerPage);    // приведём количество страниц к числу
-    const page = parseInt(state.page ?? 1);  
-    const totalFrom = parseFloat(state.totalFrom);
-    const totalTo = parseFloat(state.totalTo);              // номер страницы по умолчанию 1 и тоже число
+  const state = processFormData(new FormData(sampleTable.container));
+  const rowsPerPage = parseInt(state.rowsPerPage); // приведём количество страниц к числу
+  const page = parseInt(state.page ?? 1);
+  const totalFrom = parseFloat(state.totalFrom);
+  const totalTo = parseFloat(state.totalTo); // номер страницы по умолчанию 1 и тоже число
 
-    return {                                            // расширьте существующий return вот так
-        ...state,
-        rowsPerPage,
-        page,
-        total: [totalFrom, totalTo],
-    };
+  return {
+    // расширьте существующий return вот так
+    ...state,
+    rowsPerPage,
+    page,
+    total: [totalFrom, totalTo],
+  };
 }
 
 /**
@@ -41,53 +39,63 @@ function collectState() {
  * @param {HTMLButtonElement?} action
  */
 async function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let query = {}; // копируем для последующего изменения
-    // @todo: использование
-    // result = applySearching(result, state, action);
-    // result = applyFiltering(result, state, action);
-    // result = applySorting(result, state, action);
-    // result = applyPagination(result, state, action); 
-    const { total, items } = await api.getRecords(query);
+  let state = collectState(); // состояние полей из таблицы
+  let query = {}; // здесь будут формироваться параметры запроса
+  // другие apply*
+  query = applySearching(query, state, action);
+  query = applyFiltering(query, state, action);
+  query = applySorting(query, state, action);
+  query = applyPagination(query, state, action); // обновляем query
 
-    sampleTable.render(items )
+  const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
+
+  updatePagination(total, query); // перерисовываем пагинатор
+  sampleTable.render(items);
 }
 
-const sampleTable = initTable({
-    tableTemplate: 'table',
-    rowTemplate: 'row',
-    before: ['search', 'header', 'filter'],
-    after: ['pagination']
-}, render);
-
-// @todo: инициализация
-const applyPagination = initPagination(
-    sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
-    (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
-        const input = el.querySelector('input');
-        const label = el.querySelector('span');
-        input.value = page;
-        input.checked = isCurrent;
-        label.textContent = page;
-        return el;
-    }
+const sampleTable = initTable(
+  {
+    tableTemplate: "table",
+    rowTemplate: "row",
+    before: ["search", "header", "filter"],
+    after: ["pagination"],
+  },
+  render,
 );
 
-const applySearching = initSearching('search');
+// @todo: инициализация
+const { applyPagination, updatePagination } = initPagination(
+  sampleTable.pagination.elements, // передаём сюда элементы пагинации, найденные в шаблоне
+  (el, page, isCurrent) => {
+    // и колбэк, чтобы заполнять кнопки страниц данными
+    const input = el.querySelector("input");
+    const label = el.querySelector("span");
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+    return el;
+  },
+);
 
+const applySearching = initSearching("search");
 
-const appRoot = document.querySelector('#app');
+const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
-const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
-    sampleTable.header.elements.sortByDate,
-    sampleTable.header.elements.sortByTotal
+const applySorting = initSorting([
+  // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
+  sampleTable.header.elements.sortByDate,
+  sampleTable.header.elements.sortByTotal,
 ]);
+const { applyFiltering, updateIndexes } = initFiltering(
+  sampleTable.filter.elements,
+);
 
-// const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
-//     searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
-// });
 async function init() {
-    const indexes = await api.getIndexes();
+  const indexes = await api.getIndexes();
+
+  updateIndexes(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers,
+  });
 }
 init().then(render);
